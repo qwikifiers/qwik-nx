4e59c859e42a60ef640d94945524422b06b243fd
Dmitriy Stepanenko <33101123+dmitry-stepanenko@users.noreply.github.com>
2023-02-20 11:22:16 +0200

    test(qwik-nx): run the same basic tests for both "app" and "preset" generators (#86)

---
+++ e2e/qwik-nx-e2e/tests/application-basic-behavior.suite.ts
@@ -0,0 +1,82 @@
+import {
+  checkFilesExist,
+  ensureNxProject,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
+
+import {
+  runCommandUntil,
+  promisifiedTreeKill,
+  killPort,
+} from '@qwikifiers/e2e/utils';
+
+export function testApplicationBasicBehavior(generator: 'app' | 'preset') {
+  describe(`Basic behavior with ${generator} generator`, () => {
+    let project: string;
+
+    beforeAll(() => {
+      ensureNxProject('qwik-nx', 'dist/packages/qwik-nx');
+    });
+
+    beforeAll(async () => {
+      project = uniq('qwik-nx');
+
+      const projectNameParam =
+        generator === 'preset' ? `--qwikAppName=${project}` : project;
+
+      await runNxCommandAsync(
+        `generate qwik-nx:${generator} ${projectNameParam} --no-interactive`
+      );
+    }, 200000);
+
+    afterAll(async () => {
+      await runNxCommandAsync('reset');
+    });
+
+    it('should create qwik-nx', async () => {
+      const result = await runNxCommandAsync(`build-ssr ${project}`);
+      expect(result.stdout).toContain(
+        `Successfully ran target build-ssr for project ${project}`
+      );
+      expect(() =>
+        checkFilesExist(`dist/apps/${project}/client/q-manifest.json`)
+      ).not.toThrow();
+      expect(() =>
+        checkFilesExist(`dist/apps/${project}/server/entry.preview.mjs`)
+      ).not.toThrow();
+    }, 200000);
+
+    it('should serve application in dev mode with custom port', async () => {
+      const port = 4212;
+      const p = await runCommandUntil(
+        `run ${project}:serve --port=${port}`,
+        (output) => {
+          return output.includes('Local:') && output.includes(`:${port}`);
+        }
+      );
+      try {
+        await promisifiedTreeKill(p.pid!, 'SIGKILL');
+        await killPort(port);
+      } catch {
+        // ignore
+      }
+    }, 200000);
+
+    it('should serve application in preview mode with custom port', async () => {
+      const port = 4232;
+      const p = await runCommandUntil(
+        `run ${project}:preview --port=${port}`,
+        (output) => {
+          return output.includes('Local:') && output.includes(`:${port}`);
+        }
+      );
+      try {
+        await promisifiedTreeKill(p.pid!, 'SIGKILL');
+        await killPort(port);
+      } catch {
+        // ignore
+      }
+    }, 200000);
+  });
+}
