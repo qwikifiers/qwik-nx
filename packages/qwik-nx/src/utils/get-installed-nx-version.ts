d441f1c22d2846020ee93e02bc5eb14c1a054726
Dmitriy Stepanenko <stepanenkodmitri@gmail.com>
2023-02-22 16:47:18 +0200

    fix(qwik-nx): preset generator should check for the presense of peerDeps

--- packages/qwik-nx/src/utils/get-installed-nx-version.ts
+++ packages/qwik-nx/src/utils/get-installed-nx-version.ts
@@ -1,9 +1,21 @@
 import { readJson, Tree } from '@nrwl/devkit';
+import { PackageJson } from 'nx/src/utils/package-json';
+
+function readNxVersion(packageJson: PackageJson) {
+  return (
+    packageJson?.devDependencies?.['nx'] ??
+    packageJson?.dependencies?.['nx'] ??
+    packageJson?.devDependencies?.['@nrwl/workspace'] ??
+    packageJson?.dependencies?.['@nrwl/workspace']
+  );
+}
 
 export function getInstalledNxVersion(tree: Tree): string {
-  const pkgJson = readJson(tree, 'package.json');
-  if (pkgJson.devDependencies && pkgJson.devDependencies['@nrwl/workspace']) {
-    return pkgJson.devDependencies['@nrwl/workspace'];
+  const pkgJson: PackageJson = readJson(tree, 'package.json');
+  const version = readNxVersion(pkgJson);
+
+  if (version) {
+    return version;
   }
   throw new Error('Could not resolve nx version from the package.json');
 }
