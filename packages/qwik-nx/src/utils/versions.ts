59c32d843d2238f05ae99997a9a8032b6e1d29d4
Shai Reznik <shairez@users.noreply.github.com>
2023-03-23 16:48:36 +0200

    feat: add migration to qwik 0.23.0 (#108)
    * feat: add migration to qwik 0.23.0
    
    * chore: update versions.ts

--- packages/qwik-nx/src/utils/versions.ts
+++ packages/qwik-nx/src/utils/versions.ts
@@ -1,16 +1,16 @@
 // qwik packages
-export const qwikVersion = '~0.21.0';
-export const qwikCityVersion = '~0.5.0';
-export const qwikEslintPluginVersion = '~0.21.0';
+export const qwikVersion = '~0.23.0';
+export const qwikCityVersion = '~0.6.6';
+export const qwikEslintPluginVersion = '~0.23.0';
 
 // css preprocessors
 export const sassVersion = '~1.56.1';
 export const lessVersion = '~4.1.3';
 export const stylusVersion = '~0.59.0';
 
-export const undiciVersion = '^5.20.0';
-export const viteVersion = '~4.1.1';
-export const viteTsconfigPathsVersion = '~4.0.3';
+export const undiciVersion = '^5.21.0';
+export const viteVersion = '~4.2.1';
+export const viteTsconfigPathsVersion = '~4.0.7';
 export const vitestVersion = '^0.28.5';
 
 export const autoprefixerVersion = '~10.4.11';
@@ -25,9 +25,9 @@ export const wranglerVersion = '^2.8.0';
 export const nxCloudflareWrangler = '^2.0.0';
 
 // other
-export const eslintVersion = '~8.28.0';
+export const eslintVersion = '~8.36.0';
 export const tsEslintVersion = '~5.43.0';
-export const prettierVersion = '~2.7.1';
-export const typescriptVersion = '~4.9.3';
+export const prettierVersion = '~2.8.6';
+export const typescriptVersion = '~5.0.2';
 export const nodeFetchVersion = '~3.3.0';
-export const typesEslint = '~8.4.10';
+export const typesEslint = '~8.21.3';
