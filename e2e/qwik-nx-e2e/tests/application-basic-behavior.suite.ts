25ad8944e4b4dad4c1417e11e06d5a6c77f02fa9
Dmitriy Stepanenko <33101123+dmitry-stepanenko@users.noreply.github.com>
2023-04-02 14:01:54 +0300

    test(qwik-nx): check page responses in e2e (#100)

--- e2e/qwik-nx-e2e/tests/application-basic-behavior.suite.ts
+++ e2e/qwik-nx-e2e/tests/application-basic-behavior.suite.ts
@@ -1,3 +1,5 @@
+import 'isomorphic-fetch';
+
 import {
   checkFilesExist,
   ensureNxProject,
@@ -11,6 +13,7 @@ import {
   killPort,
   stripConsoleColors,
   killPorts,
+  DEFAULT_E2E_TIMEOUT,
 } from '@qwikifiers/e2e/utils';
 
 export function testApplicationBasicBehavior(generator: 'app' | 'preset') {
@@ -33,7 +36,7 @@ export function testApplicationBasicBehavior(generator: 'app' | 'preset') {
       await runNxCommandAsync(
         `generate qwik-nx:${generator} ${projectNameParam} --no-interactive`
       );
-    }, 200000);
+    }, DEFAULT_E2E_TIMEOUT);
 
     afterAll(async () => {
       await runNxCommandAsync('reset');
@@ -43,80 +46,164 @@ export function testApplicationBasicBehavior(generator: 'app' | 'preset') {
       expect(() => checkFilesExist(`tsconfig.base.json`)).not.toThrow();
     });
 
-    it('should create qwik-nx', async () => {
-      const result = await runNxCommandAsync(`build ${project}`);
-      expect(stripConsoleColors(result.stdout.replace(/\n|\s/g, ''))).toContain(
-        [
-          'Targets to be executed:',
-          `${project}:build.client`,
-          `${project}:build.ssr`,
-        ]
-          .join('')
-          .replace(/\n|\s/g, '')
-      );
-      expect(result.stdout).toContain(
-        `Successfully ran target build for project ${project}`
-      );
-      expect(() =>
-        checkFilesExist(`dist/apps/${project}/client/q-manifest.json`)
-      ).not.toThrow();
-      expect(() =>
-        checkFilesExist(`dist/apps/${project}/server/entry.preview.mjs`)
-      ).not.toThrow();
-    }, 200000);
+    it(
+      'should create qwik-nx',
+      async () => {
+        const result = await runNxCommandAsync(`build ${project}`);
+        expect(
+          stripConsoleColors(result.stdout.replace(/\n|\s/g, ''))
+        ).toContain(
+          [
+            'Targets to be executed:',
+            `${project}:build.client`,
+            `${project}:build.ssr`,
+          ]
+            .join('')
+            .replace(/\n|\s/g, '')
+        );
+        expect(result.stdout).toContain(
+          `Successfully ran target build for project ${project}`
+        );
+        expect(() =>
+          checkFilesExist(`dist/apps/${project}/client/q-manifest.json`)
+        ).not.toThrow();
+        expect(() =>
+          checkFilesExist(`dist/apps/${project}/server/entry.preview.mjs`)
+        ).not.toThrow();
+      },
+      DEFAULT_E2E_TIMEOUT
+    );
 
-    it('should run build with a specified configuration', async () => {
-      // TODO: cloudflare pages or custom configurations should also be tested
-      const result = await runNxCommandAsync(
-        `build ${project} --configuration=preview`
-      );
-      expect(stripConsoleColors(result.stdout.replace(/\n|\s/g, ''))).toContain(
-        [
-          'Targets to be executed:',
-          `${project}:build.client:preview`,
-          `${project}:build.ssr:preview`,
-        ]
-          .join('')
-          .replace(/\n|\s/g, '')
-      );
-      expect(result.stdout).toContain(
-        `Successfully ran target build for project ${project}`
-      );
-    }, 200000);
+    it(
+      'should run build with a specified configuration',
+      async () => {
+        // TODO: cloudflare pages or custom configurations should also be tested
+        const result = await runNxCommandAsync(
+          `build ${project} --configuration=preview`
+        );
+        expect(
+          stripConsoleColors(result.stdout.replace(/\n|\s/g, ''))
+        ).toContain(
+          [
+            'Targets to be executed:',
+            `${project}:build.client:preview`,
+            `${project}:build.ssr:preview`,
+          ]
+            .join('')
+            .replace(/\n|\s/g, '')
+        );
+        expect(result.stdout).toContain(
+          `Successfully ran target build for project ${project}`
+        );
+      },
+      DEFAULT_E2E_TIMEOUT
+    );
 
-    it('should serve application in dev mode with custom port', async () => {
-      const p = await runCommandUntil(
-        `run ${project}:serve --port=${devServerPort}`,
-        (output) => {
-          return (
-            output.includes('Local:') && output.includes(`:${devServerPort}`)
-          );
+    it(
+      'should serve application in dev mode with custom port',
+      async () => {
+        let host: string | undefined;
+        const p = await runCommandUntil(
+          `run ${project}:serve --port=${devServerPort}`,
+          (output) => {
+            host = getHostOutput(output, devServerPort);
+            return !!host;
+          }
+        );
+        await checkPageResponses(host!);
+        try {
+          await promisifiedTreeKill(p.pid!, 'SIGKILL');
+          await killPort(devServerPort);
+        } catch {
+          // ignore
         }
-      );
-      try {
-        await promisifiedTreeKill(p.pid!, 'SIGKILL');
-        await killPort(devServerPort);
-      } catch {
-        // ignore
-      }
-    }, 200000);
+      },
+      DEFAULT_E2E_TIMEOUT
+    );
 
-    it('should serve application in preview mode with custom port', async () => {
-      const p = await runCommandUntil(
-        `run ${project}:preview --port=${previewServerPort}`,
-        (output) => {
-          return (
-            output.includes('Local:') &&
-            output.includes(`:${previewServerPort}`)
-          );
+    it(
+      'should serve application in preview mode with custom port',
+      async () => {
+        let host: string | undefined;
+        const p = await runCommandUntil(
+          `run ${project}:preview --port=${previewServerPort}`,
+          (output) => {
+            host = getHostOutput(output, previewServerPort);
+            return !!host;
+          }
+        );
+        await checkPageResponses(host!);
+        try {
+          await promisifiedTreeKill(p.pid!, 'SIGKILL');
+          await killPort(previewServerPort);
+        } catch {
+          // ignore
         }
-      );
-      try {
-        await promisifiedTreeKill(p.pid!, 'SIGKILL');
-        await killPort(previewServerPort);
-      } catch {
-        // ignore
-      }
-    }, 200000);
+      },
+      DEFAULT_E2E_TIMEOUT
+    );
   });
+}
+
+async function checkPageResponses(host: string) {
+  // wait for a while to make sure everything is settled
+  await new Promise(r => setTimeout(r, 3000));
+  const domParser = new DOMParser();
+  const pages = ['', 'flower'];
+  for (const page of pages) {
+    const url = `${host}/${page}`;
+    const res = await fetch(url, { headers: { accept: 'text/html' } }).then(
+      (r) => r.text()
+    );
+    const html = domParser.parseFromString(res, 'text/html');
+    expectHtmlOnAPage(html, page);
+  }
+}
+
+function getHostOutput(output: string, port: number): string | undefined {
+  // extracts the host url from the output message
+  // the reason it's done is because it can either be in these formats:
+  // http://localhost:4200 or https://127.0.0.1:4200
+  const regexp = new RegExp(
+    `Local:(?:\\s{3})(http(?:s)?:\\/\\/(?:.+):${port})`
+  );
+  return output.match(regexp)?.[1];
+}
+
+function expectHtmlOnAPage(html: Document, page: string) {
+  switch (page) {
+    case '':
+      expectHtmlOnARootPage(html);
+      break;
+    case 'flower':
+      expectHtmlOnAFlowerPage(html);
+      break;
+  }
+}
+
+function expectHtmlOnARootPage(html: Document) {
+  expectCommonHtml(html);
+
+  expect(html.querySelector('.mindblow')?.textContent).toContain(
+    'Blow my mind'
+  );
+}
+
+function expectHtmlOnAFlowerPage(html: Document) {
+  expectCommonHtml(html);
+
+  expect(html.querySelector('input[type="range"]')).toBeTruthy();
+}
+
+function expectCommonHtml(html: Document) {
+  const headerLinks = html.querySelectorAll('header li a');
+  expect(headerLinks.length).toBe(3);
+  expect(Array.from(headerLinks).map((l) => l.textContent)).toEqual([
+    'Docs',
+    'Examples',
+    'Tutorials',
+  ]);
+  expect(html.querySelector('footer')?.textContent).toContain(
+    'Made with ♡ by Builder.io'
+  );
 }
