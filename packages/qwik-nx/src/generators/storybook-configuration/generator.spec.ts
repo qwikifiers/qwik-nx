1b86781cf0b19d888692eb7aa5864a344be9b7c2
Dmitriy Stepanenko <stepanenkodmitri@gmail.com>
2023-04-08 20:34:08 +0300

    fix(qwik-nx): storybook updates

--- packages/qwik-nx/src/generators/storybook-configuration/generator.spec.ts
+++ packages/qwik-nx/src/generators/storybook-configuration/generator.spec.ts
@@ -5,6 +5,7 @@ import { storybookConfigurationGenerator } from './generator';
 import { StorybookConfigurationGeneratorSchema } from './schema';
 import appGenerator from '../application/generator';
 import { Linter } from '@nrwl/linter';
+import { libraryGenerator } from '../library/generator';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const devkit = require('@nrwl/devkit');
@@ -68,5 +69,54 @@ describe('storybook-configuration generator', () => {
         .sort((a, b) => a.path.localeCompare(b.path))
         .map((c) => ({ path: c.path, type: c.type }))
     ).toMatchSnapshot();
+    expect(
+      appTree.read(`apps/${projectName}/.storybook/main.ts`)?.toString()
+    ).toMatchSnapshot();
+    expect(
+      appTree.read(`apps/${projectName}/.storybook/preview.ts`)?.toString()
+    ).toMatchSnapshot();
+  });
+
+  describe('should conditionally add qwikCity decorator to preview.ts', () => {
+    const libProjectName = 'test-project-lib';
+
+    beforeEach(async () => {
+      await libraryGenerator(appTree, {
+        name: libProjectName,
+        linter: Linter.None,
+        skipFormat: true,
+        strict: true,
+        style: 'css',
+        unitTestRunner: 'none',
+      });
+    });
+
+    test.each`
+      qwikCitySupport | project
+      ${'true'}       | ${libProjectName}
+      ${'false'}      | ${libProjectName}
+      ${'auto'}       | ${libProjectName}
+      ${'true'}       | ${projectName}
+      ${'false'}      | ${projectName}
+      ${'auto'}       | ${projectName}
+    `(
+      'matches the snapshot when qwikCitySupport is "$qwikCitySupport" and project is "$project"',
+      async ({ qwikCitySupport, project }) => {
+        await storybookConfigurationGenerator(appTree, {
+          ...options,
+          qwikCitySupport,
+          name: project,
+        });
+        expect(
+          appTree
+            .read(
+              `${
+                project === libProjectName ? 'libs' : 'apps'
+              }/${project}/.storybook/preview.ts`
+            )
+            ?.toString()
+        ).toMatchSnapshot();
+      }
+    );
   });
 });
