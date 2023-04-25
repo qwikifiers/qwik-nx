0b0b8c52a066e504f11321c46e96d0f58aea7d53
Dmitriy Stepanenko <33101123+dmitry-stepanenko@users.noreply.github.com>
2023-04-25 20:23:00 +0300

    feat(qwik-nx): add "excludeProjects.keepUnrelatedProjects" option for the qwikNxVite plugin (#137)

--- packages/qwik-nx/src/plugins/qwik-nx-vite.plugin.ts
+++ packages/qwik-nx/src/plugins/qwik-nx-vite.plugin.ts
@@ -10,9 +10,9 @@ import { getVendorRoots } from './utils/get-vendor-roots';
  * At this point its main purpose is to provide Nx libraries as vendor roots for the Qwik.
  * This is required in order for the optimizer to be able to work with entities imported from those libs.
  *
- * If `options.includeProjects` is not provided, `qwikNxVite` plugin will provide Qwik with paths of Nx projects,
+ * By default `qwikNxVite` plugin will provide Qwik with paths of Nx projects,
  * that are recognized as dependencies of the current one and are specified in the tsconfig.base.json.
- * this behavior is customizable: you can exclude additional projects by providing `options.excludeProjects`
+ * this behavior is customizable: you can exclude additional projects by providing `options.excludeProjects.keepUnrelatedProjects`,
  * or completely control what's included by using `options.includeProjects`
  */
 export function qwikNxVite(options?: QwikNxVitePluginOptions): Plugin {
