360f66fafe9603924940a89d5ae4b2908011a0a6
Dmitriy Stepanenko <stepanenkodmitri@gmail.com>
2023-11-01 09:32:26 +0200

    chore(repo): enforce commitlint scopes

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,15 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'qwik-nx',
+        'create-qwik-nx',
+        'repo', // anything related to managing the repo itself
+      ],
+    ],
+    'scope-empty': [2, 'never'],
+  },
 };
